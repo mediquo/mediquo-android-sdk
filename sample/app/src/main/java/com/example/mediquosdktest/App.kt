d2d454b3c2baaf5f2041ac729ab0751db580282c
Pierluigi Cifani <pcifani@theleftbit.com>
2025-03-19 12:49:36 +0100

    Add Firebase token sync in the `onCreate` function

--- sample/app/src/main/java/com/example/mediquosdktest/App.kt
+++ sample/app/src/main/java/com/example/mediquosdktest/App.kt
@@ -3,6 +3,7 @@ package com.example.mediquosdktest
 import android.app.Application
 import android.util.Log
 import com.google.firebase.FirebaseApp
+import com.google.firebase.messaging.FirebaseMessaging
 import com.mediquo.chat.MediquoAuthenticateListener
 import com.mediquo.chat.MediquoDeAuthenticateListener
 import com.mediquo.chat.MediquoInitListener
@@ -27,5 +28,21 @@ class App: Application(){
     override fun onCreate() {
         super.onCreate()
         MediquoSDK.initialize(this, getString(R.string.api_key), mediQuoInitListener)
+        if (FirebaseApp.getApps(this).isEmpty()) {
+            FirebaseApp.initializeApp(this)
+        }
+
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w("FCM", "Fetching FCM token failed", task.exception)
+                    return@addOnCompleteListener
+                }
+
+                // Token obtenido exitosamente
+                val token = task.result
+                MediquoSDK.getInstance()?.registerPushToken(token)
+                Log.d("FCM", "Firebase Token: $token")
+            }
     }
 }
